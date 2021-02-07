91edf0f48bdfbae8b0480cebccd04dd66a881ee1
chad <chad@mieasy.com>
2021-02-07 17:17:32 +0800

    成功运行并测试完成

--- nacosmicroservice/apigateway/src/main/java/com/itheima/microservice/gateway/ApiGatewayBootstrap.java
+++ nacosmicroservice/apigateway/src/main/java/com/itheima/microservice/gateway/ApiGatewayBootstrap.java
@@ -4,13 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
+@RestController
 public class ApiGatewayBootstrap {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayBootstrap.class,args);
     }
+
+//    @Autowired
+//    private WebApplicationContext webApplicationContext;
+//
+//    @GetMapping("/property")
+//    public String getProperty(){
+//        return webApplicationContext.getEnvironment().getProperty("zuul.routes.application1.path");
+//    }
 }
